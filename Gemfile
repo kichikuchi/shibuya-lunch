54892ada33cc26a5d62fd6c27a215a3b22f80b82
kichikuchi <kikuchi0111@gmail.com>
2016-04-13 01:07:24 +0900

    name, passwordのUserモデルを作った

--- Gemfile
+++ Gemfile
@@ -10,8 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'flag_shih_tzu'
-
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # gem 'unicorn'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412153003) do
+ActiveRecord::Schema.define(version: 20160412155435) do
 
   create_table "genres", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 20160412153003) do
     t.string   "tabelog_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
