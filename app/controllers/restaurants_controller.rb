22e6d8a51e9b1b12b23870186c629031967061e9
kichikuchi <kikuchi0111@gmail.com>
2016-04-22 01:17:27 +0900

    restaurant,new を追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,6 +7,18 @@ class RestaurantsController < ApplicationController
 		@restaurant = Restaurant.find(params[:id])
 	end
 
+	def new
+		@restaurant = Restaurant.new
+	end
+	def create
+		@restaurant = Restaurant.new(restaurant_params)
+		if @restaurant.save
+			redirect_to restaurants_path
+		else
+			render 'new'
+		end
+	end
+
 	private
 	def restaurant_params
 		params[:restaurant].permit(:name)
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ActiveRecord::Base
+	validates :name, presence: { message: "入力してください" }
+	validates :latitude, presence: { message: "入力してください" }
+	validates :longitude, presence: { message: "入力してください" }
+	validates :note, presence: { message: "入力してください" }
+	validates :tabelog_url, presence: { message: "入力してください" }
 end
